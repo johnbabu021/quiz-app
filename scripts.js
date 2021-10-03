35cd34d9fa0bced91f2f876a2f90d9f30a54bed6
john <johnbabu021@gmail.com>
2021-10-03 11:20:13 +0530

    storing theme

--- scripts.js
+++ scripts.js
@@ -4,11 +4,13 @@ const hamburger = document.querySelector('.hamburger')
 const hamdiv = document.querySelector('.hamburger div')
 const options = form.querySelectorAll('.option')
 const answers = document.querySelectorAll('.ans')
+const buttons = document.querySelectorAll('button')
 const question = document.querySelector('.que')
 const alertBox = document.querySelector('.alert__box')
 const alertBoxScore = document.querySelector('.alert__box span')
 const score = document.querySelector('.score')
 const totalScore = document.querySelector(".total__score")
+const themeSelector = document.querySelector('.theme_mode')
 let xhttp = new XMLHttpRequest()
 let x;
 let index = 0;
@@ -28,7 +30,7 @@ xhttp.onload = function () {
 
     if (xhttp.status === 200) {
         form.style.display = "block"
-		toggleLoading()
+        toggleLoading()
 
         options.forEach(item => {
             item.checked = false;
@@ -62,46 +64,46 @@ xhttp.send()
 
 
 const onHandleSubmit = () => {
-    var count=0;
+    var count = 0;
     (options.forEach((item) => {
         if (item.checked) {
             if (item.value === options[arrlength[0]].value) {
-                let correctAnswerId = options[arrlength[0]].value+'a';
+                let correctAnswerId = options[arrlength[0]].value + 'a';
                 showRightAnswer(correctAnswerId);
-                function showRightAnswer(){
-                document.getElementById(options[arrlength[0]].value + 'a').classList.add('correct-answer');
-                setTimeout(() => {
-                form.style.display = "none"
-                index++
-                result++
-                xhttp.open('GET', "https://opentdb.com/api.php?amount=10", true)
-                xhttp.send()
-                score.textContent = index
-                totalScore.textContent = result
-                document.getElementById(correctAnswerId).classList.remove('correct-answer');
+                function showRightAnswer() {
+                    document.getElementById(options[arrlength[0]].value + 'a').classList.add('correct-answer');
+                    setTimeout(() => {
+                        form.style.display = "none"
+                        index++
+                        result++
+                        xhttp.open('GET', "https://opentdb.com/api.php?amount=10", true)
+                        xhttp.send()
+                        score.textContent = index
+                        totalScore.textContent = result
+                        document.getElementById(correctAnswerId).classList.remove('correct-answer');
                     }, 2000);
                 }
             }
             else {
-                let correctAnswerId = options[arrlength[0]].value+'a';
-                let wrongAnswerId = item.value+'a';
+                let correctAnswerId = options[arrlength[0]].value + 'a';
+                let wrongAnswerId = item.value + 'a';
                 showRightAndWrongAnswer();
-                function showRightAndWrongAnswer(){
-                document.getElementById(correctAnswerId).classList.add('correct-answer');
-                document.getElementById(wrongAnswerId).classList.add('wrong-answer');
-                setTimeout(() => {
-                index++
-                xhttp.open('GET', "https://opentdb.com/api.php?amount=10", true)
-                xhttp.send()
-                score.textContent = index
-                document.getElementById(correctAnswerId).classList.remove('correct-answer');
-                document.getElementById(wrongAnswerId).classList.remove('wrong-answer');
-                }, 2000);
+                function showRightAndWrongAnswer() {
+                    document.getElementById(correctAnswerId).classList.add('correct-answer');
+                    document.getElementById(wrongAnswerId).classList.add('wrong-answer');
+                    setTimeout(() => {
+                        index++
+                        xhttp.open('GET', "https://opentdb.com/api.php?amount=10", true)
+                        xhttp.send()
+                        score.textContent = index
+                        document.getElementById(correctAnswerId).classList.remove('correct-answer');
+                        document.getElementById(wrongAnswerId).classList.remove('wrong-answer');
+                    }, 2000);
                 }
             }
-			setTimeout(()=>{
-				toggleLoading()
-			},2000)
+            setTimeout(() => {
+                toggleLoading()
+            }, 2000)
 
             arrlength.sort((a, b) => { return 0.5 - Math.random() })
 
@@ -112,24 +114,22 @@ const onHandleSubmit = () => {
             }
 
         }
-        else
-        {
+        else {
             count++;
         }
 
     }))
-    if(count==4)
-    {
-        alert("Please select an option!");   
+    if (count == 4) {
+        alert("Please select an option!");
     }
 
 }
 
 const toggleLoading = () => {
-	document.querySelector(".form").classList.toggle("hide");
-	document.querySelector(".form").classList.toggle("show");
-	document.querySelector(".lds-ring").classList.toggle("hide");
-	document.querySelector(".lds-ring").classList.toggle("show");
+    document.querySelector(".form").classList.toggle("hide");
+    document.querySelector(".form").classList.toggle("show");
+    document.querySelector(".lds-ring").classList.toggle("hide");
+    document.querySelector(".lds-ring").classList.toggle("show");
 
 }
 
@@ -154,7 +154,20 @@ function onHandleReset() {
     alertBox.style.display = "none"
 }
 
-function darkMode(){
+function darkMode() {
     document.body.classList.toggle("dark");
-    document.querySelector(".form").classList.toggle("dark");    
+    localStorage.setItem('theme', themeSelector.textContent)
+    themeSelector.textContent = "Light Mode"
+    document.querySelector(".form").classList.toggle("dark");
 }
+
+(function () {
+
+    const value = localStorage.getItem('theme')
+    if (value === 'Dark Mode') {
+        darkMode()
+    }
+    else {
+        localStorage.setItem('theme', 'Light Mode')
+    }
+})()
