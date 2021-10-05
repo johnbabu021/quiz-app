e09fe5d67d33c524b9080f2a1b7d4d01c9fdab91
HerryYT <34418030+HerryYT@users.noreply.github.com>
2021-10-05 16:26:54 +0200

    Fixed formatting & removed unused parameters

--- README.md
+++ README.md
@@ -2,45 +2,41 @@
 
 ![Screenshot from 2021-10-03 14-59-41](https://user-images.githubusercontent.com/58719884/135747992-942328a0-3cf4-45eb-906e-d862d2b98c09.png)
 
-
 ![Screenshot from 2021-10-03 14-58-42](https://user-images.githubusercontent.com/58719884/135747965-89c60f7b-0cb7-400f-9775-54e6f899b8b2.png)
-
-
-
 
 A quiz app build using OPENTB api and Javascript.
 
 Limited resource application
 
 ## specifications
+
     * Dark and light theme
     * Easy medium hard levels
     * Questions based on different topics
 
-
-
-
 # doubts
-  * Send a mail to the mail in my dashboard
-  * Or by creating wiki
+
+- Send a mail to the mail in my dashboard
+- Or by creating wiki
 
 # Contribution
-* Open for contribution
-* To contribute: 
-  * Fork the repo
-  * Clone the repo
-  * Make your changes
-  * Commit and push the changes
-  * Generate a Pull Request
-  * on commit name must be like #issue number
 
-## Sit back and relax while your PR gets merged ##
+- Open for contribution
+- To contribute:
+  - Fork the repo
+  - Clone the repo
+  - Make your changes
+  - Commit and push the changes
+  - Generate a Pull Request
+  - on commit name must be like #issue number
+
+## Sit back and relax while your PR gets merged
 
 ### don't just fork start it too
+
 ### smash the star button
 
 # Leave a star if you liked the app!
-
 
 ## hacktoberfest
 
--- scripts.js
+++ scripts.js
@@ -1,226 +1,224 @@
-const header = document.querySelector('.headline')
-const form = document.querySelector('.form')
-const hamburger = document.querySelector('.hamburger')
-const hamdiv = document.querySelector('.hamburger div')
-const options = form.querySelectorAll('.option')
-const answers = document.querySelectorAll('.ans')
-const buttons = document.querySelectorAll('button')
-const question = document.querySelector('.que')
-const alertBox = document.querySelector('.alert__box')
-const alertBoxScore = document.querySelector('.alert__box span')
-const score = document.querySelector('.score')
-const totalScore = document.querySelector(".total__score")
-const themeSelector = document.querySelector('.theme_mode')
+const header = document.querySelector(".headline");
+const form = document.querySelector(".form");
+const hamburger = document.querySelector(".hamburger");
+const hamdiv = document.querySelector(".hamburger div");
+const options = form.querySelectorAll(".option");
+const answers = document.querySelectorAll(".ans");
+const buttons = document.querySelectorAll("button");
+const question = document.querySelector(".que");
+const alertBox = document.querySelector(".alert__box");
+const alertBoxScore = document.querySelector(".alert__box span");
+const score = document.querySelector(".score");
+const totalScore = document.querySelector(".total__score");
+const themeSelector = document.querySelector(".theme_mode");
 
-let xhttp = new XMLHttpRequest()
-let x;
-let index = 0;
-let result = 0;
-score.textContent = index
-totalScore.textContent = result
-alertBox.style.display = "none"
-const arrlength = [0, 1, 2, 3]
-arrlength.sort((a, b) => { return 0.5 - Math.random() })
-hamburger.addEventListener('mouseover', hamFunction)
-hamburger.addEventListener('mouseout', hamFunction1)
+const xhttp = new XMLHttpRequest();
+let x, index = 0, result = 0;
+score.textContent = index;
+totalScore.textContent = result;
+alertBox.style.display = "none";
+
+const arrlength = [0, 1, 2, 3];
+arrlength.sort((_, _) => {
+  return 0.5 - Math.random();
+});
+
+hamburger.addEventListener("mouseover", hamFunction);
+hamburger.addEventListener("mouseout", hamFunction1);
 let correctAnswer;
 let submitting = false;
 
+xhttp.onload = function () {
+  x = JSON.parse(this.response);
+  xhttp.status === 200 && evalFunc();
+};
+xhttp.open("GET", "https://opentdb.com/api.php?amount=10", true);
+xhttp.send();
+
 const evalFunc = function () {
+  form.style.display = "block";
+  toggleLoading();
 
-    if (xhttp.status === 200) {
-        form.style.display = "block"
-        toggleLoading()
+  options.forEach((item) => {
+    item.checked = false;
+  });
 
-        options.forEach(item => {
-            item.checked = false;
-        })
-
-        if (x.results[index].incorrect_answers.length === 3) {
-            answers[arrlength[0]].innerHTML = x.results[index].correct_answer
-            answers[arrlength[1]].innerHTML = x.results[index].incorrect_answers[0]
-            answers[arrlength[2]].innerHTML = x.results[index].incorrect_answers[1]
-            answers[arrlength[3]].innerHTML = x.results[index].incorrect_answers[2]
-            question.innerHTML = x.results[index].question
-            correctAnswer = x.results[index].correct_answer;
-
-        }
-        else {
-            index++;
-            answers[arrlength[0]].innerHTML = x.results[index].correct_answer
-            answers[arrlength[1]].innerHTML = x.results[index].incorrect_answers[0]
-            answers[arrlength[2]].innerHTML = x.results[index].incorrect_answers[1]
-            answers[arrlength[3]].innerHTML = x.results[index].incorrect_answers[2]
-            question.innerHTML = x.results[index].question
-            correctAnswer = x.results[index].correct_answer;
-
-
-        }
-    }
-}
-
-xhttp.onload = function() {
-    x = JSON.parse(this.response)
-    evalFunc();
-}
-xhttp.open('GET', "https://opentdb.com/api.php?amount=10", true)
-xhttp.send()
-
+  if (x.results[index].incorrect_answers.length === 3) {
+    answers[arrlength[0]].innerHTML = x.results[index].correct_answer;
+    answers[arrlength[1]].innerHTML = x.results[index].incorrect_answers[0];
+    answers[arrlength[2]].innerHTML = x.results[index].incorrect_answers[1];
+    answers[arrlength[3]].innerHTML = x.results[index].incorrect_answers[2];
+    question.innerHTML = x.results[index].question;
+    correctAnswer = x.results[index].correct_answer;
+  } else {
+    index++;
+    answers[arrlength[0]].innerHTML = x.results[index].correct_answer;
+    answers[arrlength[1]].innerHTML = x.results[index].incorrect_answers[0];
+    answers[arrlength[2]].innerHTML = x.results[index].incorrect_answers[1];
+    answers[arrlength[3]].innerHTML = x.results[index].incorrect_answers[2];
+    question.innerHTML = x.results[index].question;
+    correctAnswer = x.results[index].correct_answer;
+  }
+};
 
 const onHandleSubmit = () => {
-    var count = 0;
-    if (submitting) {
-        return; // ignore if the user has already submitted the answer for this question
-    }
-    (options.forEach((item) => {
-        if (item.checked) {
-            submitting = true;
-            if (item.value === options[arrlength[0]].value) {
-                let correctAnswerId = options[arrlength[0]].value + 'a';
-                showRightAnswer(correctAnswerId);
-                function showRightAnswer() {
-                    document.getElementById(options[arrlength[0]].value + 'a').classList.add('correct-answer');
-                    setTimeout(() => {
-                        form.style.display = "none"
-                        index++
-                        result++
-                        evalFunc();
-                        score.textContent = index
-                        totalScore.textContent = result
-                        document.getElementById(correctAnswerId).classList.remove('correct-answer');
-                    }, 2000);
-                }
-            }
-            else {
-                let correctAnswerId = options[arrlength[0]].value + 'a';
-                let wrongAnswerId = item.value + 'a';
-                showRightAndWrongAnswer();
-                function showRightAndWrongAnswer() {
-                    document.getElementById(correctAnswerId).classList.add('correct-answer');
-                    document.getElementById(wrongAnswerId).classList.add('wrong-answer');
-                    setTimeout(() => {
-                        index++
-                        evalFunc();
-                        score.textContent = index
-                        document.getElementById(correctAnswerId).classList.remove('correct-answer');
-                        document.getElementById(wrongAnswerId).classList.remove('wrong-answer');
-                    }, 2000);
-                }
-            }
-            setTimeout(() => {
-                toggleLoading()
-                submitting = false;
-            }, 2000)
-
-            arrlength.sort((a, b) => { return 0.5 - Math.random() })
-
-            if (index === 10) {
-                alertBox.style.display = "block"
-                alertBoxScore.textContent = result
-
-            }
-
+  var count = 0;
+  if (submitting) {
+    return; // ignore if the user has already submitted the answer for this question
+  }
+  options.forEach((item) => {
+    if (item.checked) {
+      submitting = true;
+      if (item.value === options[arrlength[0]].value) {
+        let correctAnswerId = options[arrlength[0]].value + "a";
+        showRightAnswer(correctAnswerId);
+        function showRightAnswer() {
+          document
+            .getElementById(options[arrlength[0]].value + "a")
+            .classList.add("correct-answer");
+          setTimeout(() => {
+            form.style.display = "none";
+            index++;
+            result++;
+            evalFunc();
+            score.textContent = index;
+            totalScore.textContent = result;
+            document
+              .getElementById(correctAnswerId)
+              .classList.remove("correct-answer");
+          }, 2000);
         }
-        else {
-            count++;
+      } else {
+        let correctAnswerId = options[arrlength[0]].value + "a";
+        let wrongAnswerId = item.value + "a";
+        showRightAndWrongAnswer();
+        
+        function showRightAndWrongAnswer() {
+          document
+            .getElementById(correctAnswerId)
+            .classList.add("correct-answer");
+          document.getElementById(wrongAnswerId).classList.add("wrong-answer");
+          setTimeout(() => {
+            index++;
+            evalFunc();
+            score.textContent = index;
+            document
+              .getElementById(correctAnswerId)
+              .classList.remove("correct-answer");
+            document
+              .getElementById(wrongAnswerId)
+              .classList.remove("wrong-answer");
+          }, 2000);
         }
+      }
 
-    }))
-    if (count == 4) {
-        alert("Please select an option!");
+      setTimeout(() => {
+        toggleLoading();
+        submitting = false;
+      }, 2000);
+
+      arrlength.sort((_, _) => {
+        return 0.5 - Math.random();
+      });
+
+      if (index === 10) {
+        alertBox.style.display = "block";
+        alertBoxScore.textContent = result;
+      }
+    } else {
+      count++;
     }
-
-}
+  });
+  if (count == 4) {
+    alert("Please select an option!");
+  }
+};
 
 const toggleLoading = () => {
-    document.querySelector(".form").classList.toggle("hide");
-    document.querySelector(".form").classList.toggle("show");
-    document.querySelector(".lds-ring").classList.toggle("hide");
-    document.querySelector(".lds-ring").classList.toggle("show");
-
-}
+  document.querySelector(".form").classList.toggle("hide");
+  document.querySelector(".form").classList.toggle("show");
+  document.querySelector(".lds-ring").classList.toggle("hide");
+  document.querySelector(".lds-ring").classList.toggle("show");
+};
 
 function hamFunction() {
-
-    hamdiv.classList.add('blackhover')
+  hamdiv.classList.add("blackhover");
 }
 
 function hamFunction1() {
-
-    hamdiv.classList.remove('blackhover')
+  hamdiv.classList.remove("blackhover");
 }
 
 function onHandleReset() {
-    index = 0;
-    result = 0;
-            toggleLoading()
+  index = 0;
+  result = 0;
+  toggleLoading();
 
-    form.style.display = "none"
-    xhttp.onload = function () {
+  form.style.display = "none";
+  xhttp.onload = function () {
+    if (xhttp.status)
+      if (xhttp.status === 200) {
+        setTimeout(() => {
+          document.querySelector(".lds-ring").classList.add("hide");
+          form.style.display = "block";
+          form.classList.remove("hide");
+          form.classList.add("show");
+        }, 2000);
 
-        if (xhttp.status)
+        options.forEach((item) => {
+          item.checked = false;
+        });
 
-            if (xhttp.status === 200) {
-
-                setTimeout(() => {
-                    document.querySelector(".lds-ring").classList.add("hide");
-                    form.style.display = "block"
-                    form.classList.remove('hide')
-                    form.classList.add('show')
-                }, 2000)
-
-                options.forEach(item => {
-                    item.checked = false;
-                })
-
-                x = JSON.parse(this.response)
-                if (x.results[index].incorrect_answers.length === 3) {
-                    answers[arrlength[0]].innerHTML = x.results[index].correct_answer
-                    answers[arrlength[1]].innerHTML = x.results[index].incorrect_answers[0]
-                    answers[arrlength[2]].innerHTML = x.results[index].incorrect_answers[1]
-                    answers[arrlength[3]].innerHTML = x.results[index].incorrect_answers[2]
-                    question.innerHTML = x.results[index].question
-                    correctAnswer = x.results[index].correct_answer;
-
-                }
-                else {
-                    index++;
-                    answers[arrlength[0]].innerHTML = x.results[index].correct_answer
-                    answers[arrlength[1]].innerHTML = x.results[index].incorrect_answers[0]
-                    answers[arrlength[2]].innerHTML = x.results[index].incorrect_answers[1]
-                    answers[arrlength[3]].innerHTML = x.results[index].incorrect_answers[2]
-                    question.innerHTML = x.results[index].question
-                    correctAnswer = x.results[index].correct_answer;
-
-
-                }
-            }
-    }
-    xhttp.open('GET', "https://opentdb.com/api.php?amount=10", true)
-    xhttp.send()
-    score.textContent = index
-    totalScore.textContent = result
-    alertBox.style.display = "none"
+        x = JSON.parse(this.response);
+        if (x.results[index].incorrect_answers.length === 3) {
+          answers[arrlength[0]].innerHTML = x.results[index].correct_answer;
+          answers[arrlength[1]].innerHTML =
+            x.results[index].incorrect_answers[0];
+          answers[arrlength[2]].innerHTML =
+            x.results[index].incorrect_answers[1];
+          answers[arrlength[3]].innerHTML =
+            x.results[index].incorrect_answers[2];
+          question.innerHTML = x.results[index].question;
+          correctAnswer = x.results[index].correct_answer;
+        } else {
+          index++;
+          answers[arrlength[0]].innerHTML = x.results[index].correct_answer;
+          answers[arrlength[1]].innerHTML =
+            x.results[index].incorrect_answers[0];
+          answers[arrlength[2]].innerHTML =
+            x.results[index].incorrect_answers[1];
+          answers[arrlength[3]].innerHTML =
+            x.results[index].incorrect_answers[2];
+          question.innerHTML = x.results[index].question;
+          correctAnswer = x.results[index].correct_answer;
+        }
+      }
+  };
+  xhttp.open("GET", "https://opentdb.com/api.php?amount=10", true);
+  xhttp.send();
+  score.textContent = index;
+  totalScore.textContent = result;
+  alertBox.style.display = "none";
 }
 
 function darkMode() {
-    document.body.classList.toggle("dark");
-    localStorage.setItem('theme', themeSelector.textContent)
-    const value = localStorage.getItem('theme')
+  document.body.classList.toggle("dark");
+  localStorage.setItem("theme", themeSelector.textContent);
+  const value = localStorage.getItem("theme");
 
-    value === 'Dark Mode' ? themeSelector.textContent = "Light Mode" : themeSelector.textContent = "Dark Mode"
-    document.querySelector(".form").classList.toggle("dark");
+  value === "Dark Mode"
+    ? (themeSelector.textContent = "Light Mode")
+    : (themeSelector.textContent = "Dark Mode");
+  document.querySelector(".form").classList.toggle("dark");
 }
 
 (function () {
-    const value = localStorage.getItem('theme')
-    if (value === 'Dark Mode') {
-        darkMode()
-    }
-    else if (value === 'Light Mode') {
-        return null
-    }
-    else {
-        localStorage.setItem('theme', 'Light Mode')
-    }
-})()
+  const value = localStorage.getItem("theme");
+  if (value === "Dark Mode") {
+    darkMode();
+  } else if (value === "Light Mode") {
+    return null;
+  } else {
+    localStorage.setItem("theme", "Light Mode");
+  }
+})();
